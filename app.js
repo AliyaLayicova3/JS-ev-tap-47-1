4a3b5b2d27ae6bfab8035d9b1b83c03bd47f9892
Layicovaaliya <162481015+Layicovaaliya@users.noreply.github.com>
2024-06-12 15:55:11 +0400

    JS

---
+++ app.js
@@ -0,0 +1,27 @@
+// console.log(Math.round(Math.random()*100));
+
+
+// let a = (Math.round(Math.random()*50));
+// console.log(a);
+// let b = (Math.round(Math.random()*50))
+// console.log(b);
+// let c = a+b
+// console.log(c); 
+
+
+// let a = (1);
+// let b = (4);
+// let c = (9);
+// let d = (2);
+// let e = (4);
+// let f = (6);
+// console.log(Math.min(a,b,c,d,e,f));
+// console.log(Math.max(a,b,c,d,e,f));
+
+
+// let color = ["Mavi","Yaşıl","Qırmızı","Narıncı","Benovşe","Ağ","Sarı"]
+// color.forEach(myFunction);
+
+// function myFunction(item, index) {
+//     document.getElementById("massiv").innerHTML += index + "-ci seçim " + item + "<br>";
+// }
